20b309f829374a4f111903181a830f268f70da4d
RyanBeaumont <rbeaumon@msudenver.edu>
2021-02-10 12:05:34 -0700

    Added Create function and Main calls it now

--- Main.java
+++ Main.java
@@ -3,10 +3,10 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String filePath = "src/inventory_team2.csv";
+        String filePath = "inventory_team2.csv";
         CRUD crud = new CRUD(filePath);
+        crud.create(crud.filePath,"TESTPRODUCT","69","1.21","420","TESTSELLER");
         crud.read(crud.filePath);
-        crud.create(crud.filePath);
         crud.update(crud.filePath);
         crud.delete(crud.filePath);
     }
