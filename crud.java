1545ca60be9e7ba25ef6f6bebc44aa8b69127fa4
Diemolin <dmolina8@msudenver.edu>
2021-02-09 21:37:41 -0700

    Adding crud.java class to the Grapefruit Database

---
+++ crud.java
@@ -0,0 +1,78 @@
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
+public class CRUD {
+    String[] values;
+    String line = "";
+    public String filePath = "";
+
+
+    public CRUD(String filePath) throws FileNotFoundException {
+        this.filePath = filePath;
+        BufferedReader csvReader = new BufferedReader(new FileReader(filePath));
+        try{
+            while((line = csvReader.readLine()) != null){
+                values = line.split(",");
+                System.out.println(values[1]);
+            }
+            csvReader.close();
+        }
+        catch(IOException e){
+            e.printStackTrace();
+        }
+    }
+    public String create(String filePath){
+        String returnValue = "";
+        return returnValue;
+    }
+    public String read(String filePath) throws IOException {
+        //Initalize the returnValue
+        String returnValue = "";
+
+        //Initalize a Scanner
+        BufferedReader csvReader = new BufferedReader(new FileReader(filePath));
+
+        //Ask which column they want to read from
+        System.out.println("From what column do you want to pull info from?");
+        System.out.println("Product_ID(0), Quantity(1), Wholesale_Cost(2), Sale_Price(3), Supplier_ID(4)");
+
+        //Start Scanner
+        Scanner sc = new Scanner(System.in);
+        //Ask for column #
+        int column = sc.nextInt();
+        //Reading the leftover new line
+        sc.nextLine();
+
+        //Ask for search info
+        System.out.println("Enter info for search");
+        String search = sc.nextLine();
+
+        //Loops and searches for Search Info in the column chosen
+        while((line = csvReader.readLine()) != null){
+            values = line.split(",");
+            if (values[column].equals(search)){
+                returnValue = line;
+                break;
+            }
+        }
+        csvReader.close();
+
+        // Return returnValue
+        System.out.println(returnValue);
+        return returnValue;
+    }
+
+    public String update(String filePath){
+        String returnValue = "";
+        return returnValue;
+    }
+
+    public String delete(String filePath){
+        String returnValue = "";
+        return returnValue;
+    }
+
+}
