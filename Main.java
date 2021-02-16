ef8b94bf5113f56ab3837b2feaee8978abc6f1b3
RyanBeaumont <rbeaumon@msudenver.edu>
2021-02-15 22:55:34 -0700

    Semi Functional Update/Delete

--- Main.java
+++ Main.java
@@ -1,13 +1,12 @@
 import java.io.*;
-import java.util.Scanner;
 
 public class Main {
         public static void main(String[] args) throws IOException {
-        String filePath = "src/inventory_team2.csv";
+        String filePath = "inventory_team2.csv";
         CRUD crud = new CRUD(filePath);
         crud.read(crud.filePath);
-        crud.create(crud.filePath);
-        crud.update(crud.filePath);
-        crud.delete(crud.filePath);
+        crud.create(crud.filePath,"testproduct","1234","5678","9ABC","6969");
+        crud.update(crud.filePath,3,"AAA","AAA","AAA","AAA","AAA");
+        crud.delete(crud.filePath,2);
     }
 }
