eb238a3fc2f25cfbc2c48f18872752f7a4a3c746
[jgray52@msudenver.edu] <jg5280@gmail.com>
2021-02-26 13:17:59 -0700

    working

--- Main.java
+++ Main.java
@@ -1,142 +1,38 @@
 import java.io.*;
 import java.util.Scanner;
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+
 
 public class Main {
-        public static void main(String[] args) throws IOException {
-        String filePath = "inventory_team2.csv";
-        crud db = new crud();
-        Scanner scan = new Scanner(System.in);
-        Boolean loop = true;
+    public static void main(String[] args) throws IOException {
 
-        //Logo
-        System.out.println("Python Studio Presents...");
-        System.out.println("  #####\n#     # #####    ##   #####  ###### ###### #####  #    # # ##### \n#       #    #  #  #  #    # #      #      #    # #    # #   #   \n#  #### #    # #    # #    # #####  #####  #    # #    # #   #   \n#     # #####  ###### #####  #      #      #####  #    # #   #   \n#     # #   #  #    # #      #      #      #   #  #    # #   #   \n #####  #    # #    # #      ###### #      #    #  ####  #   #  ");
-        
+        String connectionUrl = "jdbc:sqlserver://grapefruit-mango-s1.database.windows.net:1433;"
+                + "database=Grapefruit;" + "user=GrapeAdmin;" + "password=TreeRockCar2021;" + "encrypt=true;"
+                + "trustServerCertificate=false;" + "loginTimeout=30;";
 
-        //password
-        System.out.println("Enter the Secret Password (1234): ");
-        String s = scan.nextLine();
-        System.out.println(s);
-        if(!s.equals("1234")){
-            System.out.println("Bad password, terminating...");
-            System.exit(1);
-        }
+        ResultSet resultSet = null;
 
-        while(loop){
-            //Menu
-            System.out.println("\n\nEnter the number of the function you want:\n0: Load CSV File\n1: Search for a product\n2: Save changes\n3: Exit");
-            int i = scan.nextInt();
-            scan.nextLine(); //this is here to prevent skipping prompts later on
-            switch(i){
-                case 0: //Load File
-                    System.out.println("Enter the file name to load, or press enter for <inventory_team2.csv>");
-                    String fName = scan.nextLine();
-                    if(fName.equals("")){fName = "inventory_team2.csv";}
-                    System.out.println("Loading file: "+fName);
-                    System.out.println("Replace existing contents? (Y/N)");
-                    String ans = scan.nextLine();
-                    Boolean overwrite = false;
-                    if(ans.equals("Y") || ans.equals("y")){overwrite = true;}
-                    System.out.println("Loading File...");
+        try (Connection connection = DriverManager.getConnection(connectionUrl);
+                Statement statement = connection.createStatement();) {
 
-                    try{
-                        db.load(fName,overwrite);
-                    }catch(Exception e){
-                        System.out.println("File not found :/");
-                        break;
-                    }
+            // Create and execute a SELECT SQL statement.
+            String selectSql = "SELECT quantity FROM inventory_team2 WHERE quantity = 1";
 
-                    System.out.println("Success... Database now has "+String.valueOf(db.getSize())+" Entries\n\n");
-                break;
-                case 1: //Search
-                    if(db.getSize() == 0){
-                        System.out.println("Please load data into the database");
-                        break;
-                    }
-                    System.out.println("Enter the category to search by:\n0: Product ID\n1: Quantity\n2: Wholesale Cost\n3: Sale Price\n4: Supplier ID");
-                    int searchCategory = scan.nextInt();
-                    scan.nextLine();
-                    if(searchCategory >= 0 && searchCategory <= 4){
-                        System.out.println("Word or number to search for: ");
-                        String searchWord = scan.nextLine();
-                        List<Integer> searchResults = db.search(searchCategory, searchWord);
-                        System.out.println("Search Results: ");
+            resultSet = statement.executeQuery(selectSql);
 
-                        if(searchResults.size() == 0){
-                            System.out.println("None");
-                            break;
-                        }
-
-                        //Loop through list of results and display them
-                        for(i = 0; i < searchResults.size(); i++){
-                            db.printIndex(searchResults.get(i));
-                        }
-
-                        System.out.println("---------\nWhat do you want to with this data?\n0: Change value\n1: Delete entries\n2: Nothing");
-                        int promptAns = scan.nextInt();
-                        scan.nextLine();
-                        if(promptAns == 0){
-
-                            //Replace items
-                            System.out.println("Enter the category to replace:\n0: Product ID\n1: Quantity\n2: Wholesale Cost\n3: Sale Price\n4: Supplier ID");
-                            int replaceCategory = scan.nextInt();
-                            scan.nextLine();
-                            if(replaceCategory >= 0 && replaceCategory <= 4){
-                                System.out.println("Enter the new value:");
-                                String newData = scan.nextLine();
-                                for(i = 0; i < searchResults.size(); i++){
-                                    db.replace(replaceCategory, searchResults.get(i), newData);
-                                }
-                                System.out.println("Done (made "+String.valueOf(i)+" Replacements)");
-                            }else{
-                                System.out.println("This is not a valid input... Back to main menu");
-                            }
-
-                        }else if(promptAns == 1){
-
-                            //Delete items
-                            for(i = 0; i < searchResults.size(); i++){
-                                db.delete(searchResults.get(i));
-                            }
-                            System.out.println("Done... Deleted "+String.valueOf(i)+" items");
-                        }
-
-                    }else{
-                        System.out.println("This is not a valid input... Try again");
-                    }
-                break;
-                case 2:
-                    //Save changes
-                    if(db.getSize() == 0){
-                        System.out.println("You have not modified the database yet (nothing to save)");
-                        break;
-                    }
-                    System.out.println("Enter the file name to save to, or press enter for <inventory_team2.csv>");
-                    String saveFile = scan.nextLine();
-                    if(saveFile.equals("")){saveFile = "inventory_team2.csv";}
-                    try{
-                        db.save(saveFile);
-                        System.out.println("File saved\n");
-                    }catch(Exception e){
-                        System.out.println("File doesn't exist");
-                    }
-                break;
-                default:
-                    loop = false;
-                    System.out.println("Exiting...");
-                break;
+            // Print results from select statement
+            while (resultSet.next()) {
+                System.out.println(resultSet.getString(1) + " testcorrect");
             }
-            
-            
+        } catch (SQLException e) {
+            e.printStackTrace();
         }
-        CRUD crud = new CRUD(filePath);
-        crud.read(crud.filePath);
-        crud.create(crud.filePath,"testproduct","1234","5678","9ABC","6969");
-        crud.update(crud.filePath,3,"AAA","AAA","AAA","AAA","AAA");
-        crud.delete(crud.filePath,2);
     }
+
+
+    
 }
--- Sql_Connection.java
+++ Sql_Connection.java
@@ -4,41 +4,47 @@ import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Scanner;
 
-public class Sql_Connection {
+public class Sql_Connection extends Connection {
 
     // Connect to your database.
     // Replace server name, username, and password with your credentials
-    public static void main(String[] args) {
-        String connectionUrl =
-                "jdbc:sqlserver://grapefruit-mango-s1.database.windows.net:1433;"
-                + "database=Grapefruit;"
-                + "user=GrapeAdmin;"
-                + "password=TreeRockCar2021;"
-                + "encrypt=true;"
-                + "trustServerCertificate=false;"
-                + "loginTimeout=30;";
 
-        ResultSet resultSet = null;
+    public void update() {
 
+        // after column is specifed choose what row to update
+
+        Scanner locationInput = new Scanner(System.in);  // Create a Scanner object to determine what column
+        System.out.println("what product do you want to update? Please enter product_id");
+
+        String userLocation = locationInput.nextLine();  // read and store user input into variable
+
+        // determine from user input what to specify update
+
+        Scanner columnInput = new Scanner(System.in);  // Create a Scanner object to determine what column
+        System.out.println("what about this product do you want to update? quantity wholesale_cost,sale_cost, supplier_id");
+
+        String userColumn = columnInput.nextLine();  // read and store user input into variable
+
+        Scanner updateInput = new Scanner(System.in); // new scanner to accept the input of change
+        System.out.println("What do you want to change it to");
+
+        String userInput = updateInput.nextLine(); // reads the users input of what they want to change it to
+
+
+
+        // build a string which will be used as the argument of the sql statment query
+        String updateSql = ("UPDATE inventory_team2 SET" + userColumn + "=" + userInput + "WHERE product_id = " + userLocation);
+
+
+            
+            
+         resultSet = statement.executeQuery(selectSql);
+
+         // Print results from select statement
+         while (resultSet.next()) {
+             System.out.println(resultSet.getString(1)+" testcorrect");
         
-
-        try (Connection connection = DriverManager.getConnection(connectionUrl);
-                Statement statement = connection.createStatement();) {
-            
-                    
-            
-            // Create and execute a SELECT SQL statement.
-            String selectSql = "SELECT TOP 10 Title, FirstName, LastName from SalesLT.Customer";
-            resultSet = statement.executeQuery(selectSql);
-
-            // Print results from select statement
-            while (resultSet.next()) {
-                System.out.println(resultSet.getString(2) + " " + resultSet.getString(3));
-            }
-        }
-        catch (SQLException e) {
-            e.printStackTrace();
-        }
     }
 }
