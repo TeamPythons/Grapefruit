e93cabbd2c0782bf5d65adb6914243cda304ae5e
RyanBeaumont <rbeaumon@msudenver.edu>
2021-03-02 14:50:17 -0700

    Added Prices and Complete Shopping Cart, just need to edit CSV

--- Website/app.js
+++ Website/app.js
@@ -1,12 +1,15 @@
 'use strict'
 var x= [];
 const buyButton = document.querySelector('.btn');
-
+var cost = 0;
+var prices = [10,5,15,25];
+var items = ["Grapefruit","Grape","Eggplant","Durian"];
 
 //Click Buy Button
 buyButton.addEventListener('click', function() {
     //JSON.stringify(x)
     localStorage.setItem('shoppingCart', x);
+    localStorage.setItem('cost', cost);
     window.location.href = "checkout.html";
 });
 
@@ -16,11 +19,25 @@ for (var i=0; i<lis.length; i++) {
     lis[i].addEventListener('click', orderItem, false);
 }
 
+function getElementIndex(node) {
+    var index = 0;
+    while ( (node = node.previousElementSibling) ) {
+        index++;
+    }
+    return index;
+}
+
 function orderItem() {
 
     var selectedItem = this.textContent;
     var list = document.getElementById('cartList');
     var itemName = this.textContent;
+    var itemIndex = getElementIndex(this);
+    var itemPrice = prices[itemIndex];
+    cost += itemPrice;
+    var p = document.getElementById("Price");
+    p.textContent = "$"+cost.toString();
+
     var entry = document.createElement('li');
     entry.appendChild(document.createTextNode(itemName));
     list.appendChild(entry);
